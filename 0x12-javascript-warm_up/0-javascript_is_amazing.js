3844569a282c41789b1c30595e0f50bb58c1f7aa
root <nwosuchinaza22@gmail.com>
2024-06-19 07:37:26 -0700

    0 javascript

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,10 @@
+#include <include stdio.h>
+
+/*
+ *  This is a multi-line comment
+ *  It can span multiple lines
+ **/
+
 let message;
 message = “JavaScript is amazing”; // assign a value
 console.log(message); // JavaScript is amazing
