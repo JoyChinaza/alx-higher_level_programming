24d4340adf46bcb4726920392106965a66e1415b
your username <nwosuchinaza22@gmail.com>
2024-06-20 03:21:57 -0700

    0 javascript

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-
-/*
- *  Creating a constant variable called myVar
- *  Print the value of myVar using console.log
- **/
-
 const myVar = "JavaScript is amazing";
 console.log(myVar);
