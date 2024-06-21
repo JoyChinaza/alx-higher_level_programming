f17a73441e3380e4226bd8179f0cc0752d7c4827
your username <nwosuchinaza22@gmail.com>
2024-06-20 17:54:39 -0700

    5 rectang

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,20 +1,11 @@
 #!/usr/bin/node
-class Rectangle {
-    constructor(w, h) {
-        if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
-            this.width = w;
-            this.height = h;
-        }
-    }
+const Rectangle = require('./4-rectangle');
 
-    print() {
-        if (this.width > 0 && this.height > 0) {
-            for (let i = 0; i < this.height; i++) {
-                console.log('X'.repeat(this.width));
-            }
-        }
-    }
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
 
-    rotate() {
-        if (this.width
+module.exports = Square;
 
