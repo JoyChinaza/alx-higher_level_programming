f1827ab7bd7773acbbd64a5fb48fc460a7355f4a
your username <nwosuchinaza22@gmail.com>
2024-06-20 17:43:12 -0700

    4 rectang

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,33 +1,32 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-        if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
+  }
 
-    print() {
-        if (this.width > 0 && this.height > 0) {
-            for (let i = 0; i < this.height; i++) {
-                console.log('X'.repeat(this.width));
-            }
-        }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
     }
+  }
 
-    rotate() {
-        if (this.width > 0 && this.height > 0) {
-            [this.width, this.height] = [this.height, this.width];
-        }
-    }
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
 
-    double() {
-        if (this.width > 0 && this.height > 0) {
-            this.width *= 2;
-            this.height *= 2;
-        }
-    }
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 
 module.exports = Rectangle;
-
