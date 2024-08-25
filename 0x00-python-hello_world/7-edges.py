525a8607dec1c1fa588c4ad93f01ff99226f27be
your username <nwosuchinaza22@gmail.com>
2024-08-24 22:54:06 -0700

    7 edges

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+print(word[0:3])
+print(word[7:9])
+print(word[1:8])
+print(f"First 3 letters: {(word[0:3])}")
+print(f"Last 2 letters: {(word[7:9])}")
+print(f"Middle word: {word[1:8]}")
