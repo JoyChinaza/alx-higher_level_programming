4e66c96fb88945c897e942560e5548122981af76
your username <nwosuchinaza22@gmail.com>
2024-09-06 19:21:43 -0700

    0 positive or negative

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-g == 13
-if g > 0
-print(is postive)
-elif g == 0
-print(is zero)
-elif g < 0
-print(is negative)
+if number > 0:
+    print(f"{number:d} is positive")
+elif number == 0:
+    print(f"{number:d} is zero")
+else:
+    print(f"{number:d} is negative")
